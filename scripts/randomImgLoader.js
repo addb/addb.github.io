25728fbb9bb993209f4a3fd2cd4330a2e3f6c453
Arjun Bhadra <abhadra@adobe.com>
2023-10-02 14:58:07 -0700

    Update page onload logic

--- scripts/randomImgLoader.js
+++ scripts/randomImgLoader.js
@@ -1,10 +1,8 @@
 // Update img element with id "hero-img" with the random image on page load
 const imageArray = ["res/imgs/fall10x8.jpg", "res/imgs/beetle10x8.jpg", "res/imgs/lightHouse10x8.jpg"]
 
-window.onload = () => displayRandomHeroImage(imageArray);
-
-function displayRandomHeroImage(imageArray) {
+document.addEventListener('DOMContentLoaded', function () {
     const randomNum = Math.floor(Math.random() * imageArray.length);
     document.getElementById("hero-img").src = imageArray[randomNum];
-}
+});
 
