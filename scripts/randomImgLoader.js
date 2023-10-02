4ff458e2036dc34bb6acafcff129762315d35682
Arjun Bhadra <abhadra@adobe.com>
2023-10-02 15:10:35 -0700

    Fix the image names

--- scripts/randomImgLoader.js
+++ scripts/randomImgLoader.js
@@ -1,8 +1,12 @@
 // Update img element with id "hero-img" with the random image on page load
-const imageArray = ["res/imgs/fall10x8.jpg", "res/imgs/beetle10x8.jpg", "res/imgs/lightHouse10x8.jpg"]
+const imageArray = ["res/imgs/fall10X8.jpg", "res/imgs/beetle10X8.jpg", "res/imgs/lightHouse10X8.jpg"]
 
-document.addEventListener('DOMContentLoaded', function () {
+function randomImgLoader() {
     const randomNum = Math.floor(Math.random() * imageArray.length);
     document.getElementById("hero-img").src = imageArray[randomNum];
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    randomImgLoader();
 });
 
